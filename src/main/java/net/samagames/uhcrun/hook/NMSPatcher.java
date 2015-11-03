c27b8f65646b1a0ab8715778f1ec67c8e218b0a4
Silvanosky <geekpower14@gmail.com>
2015-11-03 22:42:07 +0100

    Remove unused import

--- src/main/java/net/samagames/uhcrun/hook/NMSPatcher.java
+++ src/main/java/net/samagames/uhcrun/hook/NMSPatcher.java
@@ -8,7 +8,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.potion.PotionEffectType;
 
 import java.lang.reflect.Field;
-import java.sql.Ref;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
